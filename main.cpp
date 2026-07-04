be642b4f15e308c1c6755b57b0318d44dfd6d597
Mohammad-Khoshroo <mvkhoshroo83@gmail.com>
2026-07-04 17:42:47 +0330

    fix some bug in interfaces of systemc

--- main.cpp
+++ main.cpp
@@ -1,45 +1,34 @@
+#include  "src/MLP.hpp"
 
 class MyMLP : public AbstractMLP {
 public:
-    
-    Layer<8, 16, 1, RELU>*   hidden_layer1;
-    Layer<16, 16, 1, RELU>*  hidden_layer2;
+    Layer<8, 16, 1, ReLU>*        hidden_layer1;
+    Layer<16, 16, 1, ReLU>*       hidden_layer2;
     OutputLayer<16, 3, 1, SOFTMAX>* output_layer;
 
     sc_vector<sc_in<float>> mlp_inputs;
-    
     sc_in<unsigned int> label;
 
-    MyMLP(sc_module_name name, sc_in<unsigned int>& label_in) 
-        : AbstractMLP(name), 
+    MyMLP(sc_module_name name, sc_in<unsigned int>& label_in)
+        : AbstractMLP(name),
           mlp_inputs("mlp_inputs", 8),
           label("label") {
 
-        
         label.bind(label_in);
 
-        
-        hidden_layer1 = new Layer<8, 16, 1, RELU>("hidden1");
-        hidden_layer2 = new Layer<16, 16, 1, RELU>("hidden2");
-        output_layer  = new OutputLayer<16, 3, 1, SOFTMAX>("output", label); 
+        hidden_layer1 = new Layer<8, 16, 1, ReLU>("hidden1");
+        hidden_layer2 = new Layer<16, 16, 1, ReLU>("hidden2");
+        output_layer  = new OutputLayer<16, 3, 1, SOFTMAX>("output", label);
 
-        
         layers.push_back(hidden_layer1);
         layers.push_back(hidden_layer2);
         layers.push_back(output_layer);
 
-        
         for (int i = 0; i < 8; ++i) {
             hidden_layer1->get_input_port(i).bind(mlp_inputs[i]);
         }
 
-        
         build_and_connect();
-    }
-
-    void forward() override {
-        
-        
     }
 
     ~MyMLP() {
--- src/MLP.hpp
+++ src/MLP.hpp
@@ -1,15 +1,16 @@
 #ifndef MLP_STRUCT
 #define MLP_STRUCT
 
-
 #include <systemc.h>
 #include <vector>
 #include <memory>
-#include "layer.hpp" 
+#include <cassert>
+#include "layer.hpp"
+
 class AbstractMLP : public sc_module {
 protected:
-    
-    std::vector<LayerBase*> layers; 
+    std::vector<LayerBase*> layers;
+    std::vector<sc_vector<sc_signal<float>>*> inter_layer_signals;
 
 public:
     SC_HAS_PROCESS(AbstractMLP);
@@ -25,13 +26,22 @@ public:
 
             assert(outputs_count == next_inputs_count && "Layer size mismatch!");
 
+            std::string sig_name = "sig_" + std::to_string(i) + "_" + std::to_string(i+1);
+            auto* sigs = new sc_vector<sc_signal<float>>(sig_name.c_str(), outputs_count);
+            inter_layer_signals.push_back(sigs);
+
             for (int j = 0; j < outputs_count; ++j) {
-                next_layer->get_output_port(j).bind(current_layer->get_output_port(j)); 
+                current_layer->get_output_port(j).bind((*sigs)[j]);
+                next_layer->get_input_port(j).bind((*sigs)[j]);
             }
         }
     }
 
-    virtual void forward() = 0; 
+    virtual void forward() {}
+
+    virtual ~AbstractMLP() {
+        for (auto* s : inter_layer_signals) delete s;
+    }
 };
 
 #endif // MLP_STRUCT
